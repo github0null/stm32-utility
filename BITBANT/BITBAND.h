1d6dfbbeade801d846490953162ce8ef738486b2
github0null <2584456014@qq.com>
2020-01-10 18:49:05 +0800

    update

--- BITBANT/BITBAND.h
+++ BITBANT/BITBAND.h
@@ -1,7 +1,7 @@
 #ifndef _H_BIT_BAND_
 #define _H_BIT_BAND_
 
-#define BITBAND(addr, bitnum) ((addr & 0xF0000000U) + 0x02000000U + ((addr & 0xFFFFFU) << 5U) + (((bitnum) & 0xFU) << 2U))
+#define BITBAND(addr, bitnum) ((addr & 0xF0000000U) + 0x02000000U + ((addr & 0xFFFFFU) << 5U) + (((bitnum)&0xFU) << 2U))
 #define MEM_ADDR(addr) *((volatile unsigned long *)(addr))
 #define BIT_ADDR(addr, bitnum) MEM_ADDR(BITBAND(addr, bitnum))
 
@@ -9,26 +9,13 @@
 #include <stm32f10x.h>
 
 //IO口地址映射
-#define GPIOA_ODR_Addr (GPIOA_BASE + 20) //0x40020014
-#define GPIOB_ODR_Addr (GPIOB_BASE + 20) //0x40020414
-#define GPIOC_ODR_Addr (GPIOC_BASE + 20) //0x40020814
-#define GPIOD_ODR_Addr (GPIOD_BASE + 20) //0x40020C14
-#define GPIOE_ODR_Addr (GPIOE_BASE + 20) //0x40021014
-#define GPIOF_ODR_Addr (GPIOF_BASE + 20) //0x40021414
-#define GPIOG_ODR_Addr (GPIOG_BASE + 20) //0x40021814
-#define GPIOH_ODR_Addr (GPIOH_BASE + 20) //0x40021C14
-#define GPIOI_ODR_Addr (GPIOI_BASE + 20) //0x40022014
+#define GPIOA_ODR_Addr (GPIOA_BASE + 12)
+#define GPIOB_ODR_Addr (GPIOB_BASE + 12)
+#define GPIOC_ODR_Addr (GPIOC_BASE + 12)
 
-#define GPIOA_IDR_Addr (GPIOA_BASE + 16) //0x40020010
-#define GPIOB_IDR_Addr (GPIOB_BASE + 16) //0x40020410
-#define GPIOC_IDR_Addr (GPIOC_BASE + 16) //0x40020810
-#define GPIOD_IDR_Addr (GPIOD_BASE + 16) //0x40020C10
-#define GPIOE_IDR_Addr (GPIOE_BASE + 16) //0x40021010
-#define GPIOF_IDR_Addr (GPIOF_BASE + 16) //0x40021410
-#define GPIOG_IDR_Addr (GPIOG_BASE + 16) //0x40021810
-#define GPIOH_IDR_Addr (GPIOH_BASE + 16) //0x40021C10
-#define GPIOI_IDR_Addr (GPIOI_BASE + 16) //0x40022010
-
+#define GPIOA_IDR_Addr (GPIOA_BASE + 8)
+#define GPIOB_IDR_Addr (GPIOB_BASE + 8)
+#define GPIOC_IDR_Addr (GPIOC_BASE + 8)
 
 #define PAout(n) BIT_ADDR(GPIOA_ODR_Addr, n) //输出
 #define PAin(n) BIT_ADDR(GPIOA_IDR_Addr, n)  //输入
@@ -38,23 +25,5 @@
 
 #define PCout(n) BIT_ADDR(GPIOC_ODR_Addr, n) //输出
 #define PCin(n) BIT_ADDR(GPIOC_IDR_Addr, n)  //输入
-
-#define PDout(n) BIT_ADDR(GPIOD_ODR_Addr, n) //输出
-#define PDin(n) BIT_ADDR(GPIOD_IDR_Addr, n)  //输入
-
-#define PEout(n) BIT_ADDR(GPIOE_ODR_Addr, n) //输出
-#define PEin(n) BIT_ADDR(GPIOE_IDR_Addr, n)  //输入
-
-#define PFout(n) BIT_ADDR(GPIOF_ODR_Addr, n) //输出
-#define PFin(n) BIT_ADDR(GPIOF_IDR_Addr, n)  //输入
-
-#define PGout(n) BIT_ADDR(GPIOG_ODR_Addr, n) //输出
-#define PGin(n) BIT_ADDR(GPIOG_IDR_Addr, n)  //输入
-
-#define PHout(n) BIT_ADDR(GPIOH_ODR_Addr, n) //输出
-#define PHin(n) BIT_ADDR(GPIOH_IDR_Addr, n)  //输入
-
-#define PIout(n) BIT_ADDR(GPIOI_ODR_Addr, n) //输出
-#define PIin(n) BIT_ADDR(GPIOI_IDR_Addr, n)  //输入
 
 #endif
